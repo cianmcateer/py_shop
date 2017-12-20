540c20a549176eccef50b99a8712014f7d1b44ba
cianmcateer <cianmcateer@gmail.com>
2017-12-20 13:07:06 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+
+print("Welcome to pyshop")
+print("Enter your name and password")
+
+name = raw_input("Name")
+password = raw_input("Password")
+def init(name, password):
+    print("Welcome " + str(name))
+
+init(name, password)
